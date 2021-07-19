0c1b4bbc71fa6a98ec1c14ed5ce5ebb7c7fd42e6
hbluestar27 <hbluestar@outlook.com>
2021-07-19 08:13:48 +0200

    update mail template

--- resources/views/mails/user/reset.blade.php
+++ resources/views/mails/user/reset.blade.php
@@ -7,22 +7,22 @@
 	</style>
 	<body>
         <div class="mail-con" style="text-align: center;">
-            <h3><b>リクエストを受け取りました</b></h3>
-            <p style="font-size: 18px;">今すぐパスワードをリセットできます。</p>
-            
-            <a href="#" style="color:#FFF; background: red; padding: 10px 20px; border-radius: 50%;">パスワードをリセットする</a>
-
-            <p style="font-size: 18px;">
+            <p style="font-size: 22px;"><b>リクエストを受け取りました</b></p>
+            <p style="font-size: 16px; padding: 0; margin: 0; line-height: 1.75;">今すぐパスワードをリセットできます。</p>
+            <br/>
+            <a href="#" style="color:#FFF; background: red; padding: 10px 20px; border-radius: 30px; font-weight: bold; text-decoration: none; font-size: 16px;">パスワードをリセットする</a>
+            <br/>
+            <p style="font-size: 16px; padding: 0; margin: 0; line-height: 1.75;">
                 パスワードをリセットしてから24時間以内に、新しいノヾスワードを 設定してください。
                 24時間を経過すると、再度パスワードのリセッ トをリクエストする必要がありますのでご注意ください。
             </p>
-            <p style="font-size: 18px;">
+            <p style="font-size: 16px; padding: 0; margin: 0; line-height: 1.75;">
                 新しいパスワードのリクエストをした覚えがない場合は、このメー ルを無視していただいて結構です。
             </p>
-
-            <p style="font-size: 14px;">このメールはyoshiambition18@gmail.com宛てに送信されました</p>
-            <p style="font-size: 14px; font-weight: 500;">メール設定を管理する•自分のアカウントではない</p>
-            <p style="font-size: 14px; font-weight: 500;">ヘルプセンター • プライバシーポリシー • 利用規約</p>
+            <br/>
+            <p style="font-size: 14px; padding: 0; margin: 0; line-height: 1.75;">このメールはyoshiambition18@gmail.com宛てに送信されました</p>
+            <p style="font-size: 14px; padding: 0; margin: 0; line-height: 1.75; font-weight: 500;">メール設定を管理する•自分のアカウントではない</p>
+            <p style="font-size: 14px; padding: 0; margin: 0; line-height: 1.75; font-weight: 500;">ヘルプセンター • プライバシーポリシー • 利用規約</p>
         </div>
 	</body>
 </html>

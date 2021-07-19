555e30e4ac4b508bfc8d8eed8369ddc3299f6d80
hbluestar27 <hbluestar@outlook.com>
2021-07-19 08:23:04 +0200

    update reset template

--- resources/views/mails/user/reset.blade.php
+++ resources/views/mails/user/reset.blade.php
@@ -6,20 +6,22 @@
 	<style>
 	</style>
 	<body>
-        <div class="mail-con" style="text-align: center;">
-            <p style="font-size: 22px;"><b>リクエストを受け取りました</b></p>
+        <div style="text-align: center; max-width: 520px; margin: auto;">
+            <p style="font-size: 22px; margin: 20px 0;"><b>リクエストを受け取りました</b></p>
             <p style="font-size: 16px; padding: 0; margin: 0; line-height: 1.75;">今すぐパスワードをリセットできます。</p>
-            <br/>
-            <a href="#" style="color:#FFF; background: red; padding: 10px 20px; border-radius: 30px; font-weight: bold; text-decoration: none; font-size: 16px;">パスワードをリセットする</a>
-            <br/>
+            
+            <a href="#" style="margin: 30px 0; color:#FFF; background: red; padding: 10px 20px; border-radius: 30px; font-weight: bold; text-decoration: none; font-size: 16px; display: inline-block;">
+                パスワードをリセットする
+            </a>
+            
             <p style="font-size: 16px; padding: 0; margin: 0; line-height: 1.75;">
-                パスワードをリセットしてから24時間以内に、新しいノヾスワードを 設定してください。
-                24時間を経過すると、再度パスワードのリセッ トをリクエストする必要がありますのでご注意ください。
+                パスワードをリセットしてから24時間以内に、新しいノヾスワードを設定してください。
+                24時間を経過すると、再度パスワードのリセットをリクエストする必要がありますのでご注意ください。
             </p>
-            <p style="font-size: 16px; padding: 0; margin: 0; line-height: 1.75;">
-                新しいパスワードのリクエストをした覚えがない場合は、このメー ルを無視していただいて結構です。
+            <p style="font-size: 16px; padding: 0; margin: 0 0 30px; line-height: 1.75;">
+                新しいパスワードのリクエストをした覚えがない場合は、このメールを無視していただいて結構です。
             </p>
-            <br/>
+            
             <p style="font-size: 14px; padding: 0; margin: 0; line-height: 1.75;">このメールはyoshiambition18@gmail.com宛てに送信されました</p>
             <p style="font-size: 14px; padding: 0; margin: 0; line-height: 1.75; font-weight: 500;">メール設定を管理する•自分のアカウントではない</p>
             <p style="font-size: 14px; padding: 0; margin: 0; line-height: 1.75; font-weight: 500;">ヘルプセンター • プライバシーポリシー • 利用規約</p>

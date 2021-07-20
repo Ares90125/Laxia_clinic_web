a91bf176afde3e3d609a21eda567a2ee1fd87b80
hbluestar27 <hbluestar@outlook.com>
2021-07-20 16:52:31 +0200

    update global filter

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -5,12 +5,12 @@
       <!-- <form > -->
         <input type="text" v-model="search_name" name="name" placeholder="名前・電話番号・予約IDで検索" @keyup='add_todo_keyup' @focus="focusSearch" @blur="unfocusSearch" />
         <button v-if="search_name" class="b-colse" @click="handleClose"><i class="bi bi-x"></i></button>
-        <button  :class="{ focus_btn: focus_search }" @click="goPatient" >
+        <!-- <button  :class="{ focus_btn: focus_search }" @click="goPatient" >
           <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M8.25 14.25C11.5637 14.25 14.25 11.5637 14.25 8.25C14.25 4.93629 11.5637 2.25 8.25 2.25C4.93629 2.25 2.25 4.93629 2.25 8.25C2.25 11.5637 4.93629 14.25 8.25 14.25Z" stroke="#767676" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
               <path d="M15.7498 15.7498L12.4873 12.4873" stroke="#767676" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
           </svg>
-        </button>
+        </button> -->
       <!-- </form> -->
     </div>
     <div class="header-right">

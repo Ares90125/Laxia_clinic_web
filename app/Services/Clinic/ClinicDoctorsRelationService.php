8b908ec920e23185194b0717c7b24677e6221996
hbluestar27 <hbluestar@outlook.com>
2021-08-11 09:57:55 +0200

    fix doctor page bug

--- app/Services/Clinic/ClinicDoctorsRelationService.php
+++ app/Services/Clinic/ClinicDoctorsRelationService.php
@@ -30,31 +30,35 @@ class ClinicDoctorsRelationService
 
   public function get($search, $clinic_id) {
     if(isset($search['status']) && $search['status'] != 0){
-      $query = ClinicDoctorsRelation::where('clinic_doctors_relation.clinic_id', $clinic_id)        
-      ->join('doctors', 'doctors.doctor_id', '=', 'clinic_doctors_relation.doctor_id')
+      $query = ClinicDoctorsRelation::join('doctors', 'doctors.doctor_id', '=', 'clinic_doctors_relation.doctor_id')
+      ->where('clinic_doctors_relation.clinic_id', $clinic_id)
       ->where('doctors.job_id', '=', $search['status'])
       ->get();
     }else if(isset($search['q'])) {
-      $query = ClinicDoctorsRelation::where('clinic_doctors_relation.clinic_id', $clinic_id)        
-        ->join('doctors', 'doctors.doctor_id', '=', 'clinic_doctors_relation.doctor_id')
-        ->where('doctors.kata_name', 'LIKE', "%{$search['q']}%")
-        ->orWhere('doctors.hira_name', 'LIKE', "%{$search['q']}%")
+      $query = ClinicDoctorsRelation::join('doctors', 'doctors.doctor_id', '=', 'clinic_doctors_relation.doctor_id')      
+        ->where('clinic_doctors_relation.clinic_id', $clinic_id)
+        ->where(function($query) use ($search) {
+          $query->where('doctors.kata_name', 'LIKE', "%{$search['q']}%")
+                ->orWhere('doctors.hira_name', 'LIKE', "%{$search['q']}%");
+        })
         ->get();
     }else{
-      $query = ClinicDoctorsRelation::where('clinic_doctors_relation.clinic_id', $clinic_id)
-        ->join('doctors', 'doctors.doctor_id', '=', 'clinic_doctors_relation.doctor_id')
+      $query = ClinicDoctorsRelation::join('doctors', 'doctors.doctor_id', '=', 'clinic_doctors_relation.doctor_id')
+        ->where('clinic_doctors_relation.clinic_id', $clinic_id)
         ->get();
     }
     return $query;
   }
 
   public function getMemberCount($clinic_id, $request = array()) {
-    $query = ClinicDoctorsRelation::where('clinic_doctors_relation.clinic_id', $clinic_id)
-      ->join('doctors', 'doctors.doctor_id', '=', 'clinic_doctors_relation.doctor_id');
+    $query = ClinicDoctorsRelation::join('doctors', 'doctors.doctor_id', '=', 'clinic_doctors_relation.doctor_id')
+      ->where('clinic_doctors_relation.clinic_id', $clinic_id);
 
     if(isset($request['q'])) {
-      $query->where('doctors.kata_name', 'LIKE', "%{$request['q']}%")
-        ->orWhere('doctors.hira_name', 'LIKE', "%{$request['q']}%");
+      $query->where(function($query) use ($request) {
+        $query->where('doctors.kata_name', 'LIKE', "%{$request['q']}%")
+              ->orWhere('doctors.hira_name', 'LIKE', "%{$request['q']}%");
+      });
     }
 
     $result = $query->get()->count();

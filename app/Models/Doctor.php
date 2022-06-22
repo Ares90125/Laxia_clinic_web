e4f3f91834d125b8acd85915161b71dee8d195bc
Ares90125 <trofimovigory0824@gmail.com>
2022-06-22 23:04:15 +0200

    correct doctor php

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -23,7 +23,6 @@ class Doctor extends Model
    * @var array
    */
   protected $fillable = [
-    'clinic_id',
     'user_id',
     'kata_name',
     'hira_name',
@@ -49,6 +48,179 @@ class Doctor extends Model
     'created_at',
     'updated_at',
   ];
+
+  public function getFirebaseKeyAttribute()
+  {
+    if ($this->user()->count()) {
+      return $this->user()->first()->firebase_key;
+    }
+    return null;
+  }
+
+  public function user()
+  {
+    return $this->belongsTo(User::class);
+  }
+
+  public function getNameAttribute()
+  {
+    return $this->user()->first()->name;
+  }
+  public function getClinicAttribute()
+  {
+    return $this->clinic()->first();
+  }
+  public function getEmailAttribute()
+  {
+    return $this->user()->first()->email;
+  }
+
+  public function getRoleAttribute()
+  {
+    return $this->user()->first()->role;
+  }
+
+  public function images()
+  {
+    return $this->morphMany(Attachment::class, 'attachable');
+  }
+
+  public function job()
+  {
+    return $this->belongsTo(Job::class, 'job_id');
+  }
+
+  public function getJobNameAttribute()
+  {
+    $job = $this->job()->where('id', $this->job_id)->first();
+    return $job ? $job->name : null;
+  }
+
+
+  public function speciality0()
+  {
+    return $this->belongsTo(Speciality::class, 'spec0');
+  }
+
+  public function getSpec0NameAttribute()
+  {
+    $speciality = $this->speciality0()->where('id', $this->spec0)->first();
+
+    if(empty($speciality)) return null;
+
+    $parent = Speciality::where('id', $speciality->parent_id)->first();
+    $parent_name = empty($parent) ? '' : $parent->name . ' / ';
+
+    return $speciality ? $parent_name . $speciality->name : null;
+  }
+
+
+  public function speciality1()
+  {
+    return $this->belongsTo(Speciality::class, 'spec1');
+  }
+
+  public function getSpec1NameAttribute()
+  {
+    $speciality = $this->speciality1()->where('id', $this->spec1)->first();
+
+    if(empty($speciality)) return null;
+
+    $parent = Speciality::where('id', $speciality->parent_id)->first();
+    $parent_name = empty($parent) ? '' : $parent->name . ' / ';
+
+    return $speciality ? $parent_name . $speciality->name : null;
+  }
+
+
+  public function speciality2()
+  {
+    return $this->belongsTo(Speciality::class, 'spec2');
+  }
+
+  public function getSpec2NameAttribute()
+  {
+    $speciality = $this->speciality2()->where('id', $this->spec2)->first();
+
+    if(empty($speciality)) return null;
+
+    $parent = Speciality::where('id', $speciality->parent_id)->first();
+    $parent_name = empty($parent) ? '' : $parent->name . ' / ';
+
+    return $speciality ? $parent_name . $speciality->name : null;
+  }
+  public function getAvgRateAttribute()
+  {
+     $avg_rate=0;
+     $diaries=$this->diaries()->get();
+     $count=count($diaries);
+     for($i=0;$i< $count;$i++){
+        $avg_rate+=$diaries[$i]['ave_rate'];
+     }
+     if($count){
+        return $avg_rate/$count;
+     }
+     return 0;
+  }
+  public function getDiariesCountAttribute()
+  {
+    return $this->diaries()->count();
+  }
+  public function diaries() {
+    return $this->hasMany(Diary::class);
+  }
+
+  public function viewers()
+  {
+    return $this->morphToMany(Patient::class, 'viewable', 'viewables','viewable_id', 'patient_id');
+  }
+
+  public function likers()
+  {
+      return $this->morphToMany(Patient::class, 'likeable');
+  }
+
+  public function getLikesCountAttribute()
+  {
+      return $this->likers()->count();
+  }
+
+  public function getIsLikeAttribute()
+  {
+      $currentUser = auth()->guard('patient')->user();
+      if (!$currentUser || !isset($currentUser->patient)) return false;
+      $likerIds = $this->likers()
+        ->get()
+        ->pluck('id')
+        ->toArray();
+      return in_array($currentUser->patient->id, $likerIds);
+  }
+
+  public function favoriters()
+  {
+    return $this->morphToMany(Patient::class, 'favoriable', 'favorites');
+  }
+
+  public function getFavoritersCountAttribute()
+  {
+    return $this->favoriters()->count();
+  }
+
+  public function getIsFavoriteAttribute()
+  {
+    $currentUser = auth()->guard('patient')->user();
+    if (!$currentUser || !($currentUser->patient)) return false;
+    $favoriterIds = $this->favoriters()
+      ->get()
+      ->pluck('id')
+      ->toArray();
+    return in_array($currentUser->patient->id, $favoriterIds);
+  }
+
+  public function getViewsCountAttribute()
+  {
+    return $this->viewers()->count();
+  }
 
   public function clinic() {
     return $this->belongsTo(Clinic::class);
